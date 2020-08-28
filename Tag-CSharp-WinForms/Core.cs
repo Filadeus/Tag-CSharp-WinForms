fca9f802bf380d0d1aafc2d2e598d2f49d993886
Filadeus <danilefimowscky@gmail.com>
2020-08-29 00:05:49 +0300

    Continuing.

---
+++ Tag-CSharp-WinForms/Core.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tag_CSharp_WinForms
+{
+    class Core
+    {
+        int size;
+        int[,] gameMap;
+        int blankX, blankY;
+        static Random random = new Random();
+
+        public Core(int size)
+        {
+            this.size = size;
+            gameMap = new int[size, size];
+        }
+
+        public void initGame()
+        {
+            for (int x = 0; x < size; x++)
+                for (int y = 0; y < size; y++)
+                    gameMap[x, y] = coordsToPos(x, y) + 1;
+
+            blankX = size - 1;
+            blankY = size - 1;
+            gameMap[blankX, blankY] = 0;
+        }
+
+        public int getNumber(int pos)
+        {
+            int x, y;
+            posToCoords(pos, out x, out y);
+            if (x < 0 || x >= size) return 0;
+            if (y < 0 || y >= size) return 0;
+            return gameMap[x, y];
+        }
+
+        public void shiftButton(int pos)
+        {
+            int x, y;
+            posToCoords(pos, out x, out y);
+
+            if(Math.Abs(blankX - x) + Math.Abs(blankY - y) != 1)
+            {
+                return;
+            }
+
+            gameMap[blankX, blankY] = gameMap[x, y];
+            gameMap[x, y] = 0;
+            blankX = x;
+            blankY = y;
+        }
+
+        public void randomShift()
+        {
+            int pos = random.Next(0, 4);
+
+        }
+
+        private int coordsToPos(int x, int y)
+        {
+            return x * size + y;
+        }
+         
+        private void posToCoords(int position, out int x, out int y)
+        {
+            y = position % size;
+            x = position / size;
+        }
+    }
+}
--- Tag-CSharp-WinForms/formGame.cs
+++ Tag-CSharp-WinForms/formGame.cs
@@ -12,9 +12,62 @@ namespace Tag_CSharp_WinForms
 {
     public partial class formGame : Form
     {
+        Core game;
         public formGame()
         {
             InitializeComponent();
+            game = new Core(4);
+        }
+        
+        private Button button(int pos)
+        {
+            switch (pos)
+            {
+                case 0: return button0;
+                case 1: return button1;
+                case 2: return button2;
+                case 3: return button3;
+                case 4: return button4;
+                case 5: return button5;
+                case 6: return button6;
+                case 7: return button7;
+                case 8: return button8;
+                case 9: return button9;
+                case 10: return button10;
+                case 11: return button11;
+                case 12: return button12;
+                case 13: return button13;
+                case 14: return button14;
+                case 15: return button15;
+                default: return null;
+            }
+        }
+        
+        private void fill()
+        {
+            for(int pos = 0; pos < 16; pos++)
+            {
+                button(pos).Text = game.getNumber(pos).ToString();
+                button(pos).Visible = game.getNumber(pos) > 0;
+            }
+        }
+
+        private void button0_Click(object sender, EventArgs e)
+        {
+            int buttonPressed = Convert.ToInt32(((Button)sender).Tag);
+            game.shiftButton(buttonPressed);
+            fill();
+        }
+
+        private void menuStartGame_Click(object sender, EventArgs e)
+        {
+            game.initGame();
+            fill();
+        }
+
+        private void menuGameExit_Click(object sender, EventArgs e)
+        {
+            
         }
     }
 }
